1f540f316284c776149c0481d059efad5e9c5c82
kalea <kalea@uw.edu>
2017-08-09 13:28:50 -0700

    added documentation for AK_nanPad function

--- AK_nanPad.m
+++ AK_nanPad.m
@@ -1,17 +1,31 @@
-function [ nanPadMat ] = AK_nanPad( Mat, Dimensions, iStart )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [ nanPadMat ] = AK_nanPad( mat, dimensions, insertMatCoords )
+%AK_nanPad returns a nan-padded version of the given matrix with the given
+%dimensions.
+%   INPUT:
+%       mat: the matrix to be nan-padded
+%       dimensions: the desired dimensions (number of rows, columns, etc.
+%           desired) for the output matrix
+%       insertMatCoords: the coordinates designating the upper left corner
+%           of where the given matrix (mat) should be inserted into the
+%           output matrix; should match the size of dimensions; defaults to
+%           ones
+%   OUTPUT:
+%       nanPadMat: a nan-padded version of the given matrix, meeting the
+%           specifications of the function arguments
 
 % check inputs
-if nargin < 3; 
-    error('AK_nanPad requires three arguments')
+if nargin < 2; 
+    error('AK_nanPad requires at least two arguments.')
+end
+if nargin < 3
+    insertMatCoords = ones(size(dimensions));
 end
 
 % create function output
-nanPadMat = nan(Dimensions);
+nanPadMat = nan(dimensions);
 
 % insert Matrix into output
-nanPadMat(iStart(1):length(Mat(:,1)),iStart(2):length(Mat(1,:))) = Mat;
+nanPadMat(insertMatCoords(1):length(mat(:,1)),insertMatCoords(2):length(mat(1,:))) = mat;
 
 end
 
